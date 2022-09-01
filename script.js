b8985b4932b82fbf2e46e1e21c1c67a0966cf477
Bruna <brunaalmeida075@gmail.com>
2022-09-01 20:33:34 -0300

    finalização tópico 4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function criaBotao(){
     const botao = document.createElement('button');
     botao.id ='button-random-color';
     botao.innerText='Cores aleatórias';
-    botao.addEventListener('click', gerarCor);
+    botao.addEventListener('click', atribuicao);
     const pai =document.getElementById('color-palette')
     pai.appendChild(botao)
 }
@@ -12,58 +12,18 @@ function gerarCor(){
     let cor = '#'
     
     for(let gerar=0;gerar<4;gerar+=1){
-      cor += num[Math.floor(Math.random() * 9)];
+      cor += num[Math.floor(Math.random() * 16)];
     }
-    return cor
+    if(cor !== '#FFFFFF'){
+        return cor;
+    }
 }
-function atribuicao(color){
-    const corr = document.getElementById('black');
-    const m = corr.style.backgroundColor=color;
-    return m;
+function atribuicao(){
+    const cor1 = document.getElementById('black').style.backgroundColor='black';
+    const cor2= document.getElementById('purple').style.backgroundColor=gerarCor();
+    const cor3= document.getElementById('blue').style.backgroundColor=gerarCor();
+    const cor4=document.getElementById('green').style.backgroundColor=gerarCor();
 }
-atribuicao('red');
 
 
-// function arrayCores(){
-//     const black = document.getElementById('black');
-//     black.style.backgroundColor = 'black';
-//     const purple = document.getElementById('purple');
-//     purple.style.backgroundColor = 'purple';
-//     const blue = document.getElementById('blue');
-//     blue.style.backgroundColor = 'blue'
-//     const green = document.getElementById('green');
-//     green.style.backgroundColor = 'green'
-//     const cores =[black,purple,blue,green];
-//     return cores;
-  
-// }
-
-// function botaoColor(){
-//     const black = arrayCores()[0];
-//     const purple = arrayCores()[1];
-//     const blue = arrayCores()[2];
-//     const green = arrayCores()[3];
-//     const cores=[black,purple,blue,green]
-   
-//     if( black){
-//         // return paleta;
-//      console.log(black)
-//     }
-//     if(purple){
-//         // return paleta;
-//         console.log(purple)
-//     }
-//     if(blue){
-//         // return paleta;
-//         console.log(blue)
-//     }
-//     if(green){
-//         // return paleta;
-//         console.log(green)
-//     }
-    
-
-// }
-// localStorage.setItem('cor',botaoColor)
-// localStorage.getItem(key)
 
