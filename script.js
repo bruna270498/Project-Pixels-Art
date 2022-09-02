8e9c86ccb759aff21b3f0c2cf9ae76e1af34b36e
Bruna <brunaalmeida075@gmail.com>
2022-09-02 14:56:53 -0300

    resolução do topico 6

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-function criaBotao(){
+function criaBotao() {
     const botao = document.createElement('button');
-    botao.id ='button-random-color';
-    botao.innerText='Cores aleatórias';
+    botao.id = 'button-random-color';
+    botao.innerText = 'Cores aleatórias';
     botao.addEventListener('click', atribuicao);
-    const pai =document.getElementById('color-palette')
-    pai.appendChild(botao)
+    const pai = document.getElementById('color-palette');
+    pai.appendChild(botao);
 }
 criaBotao();
-function gerarCor(){
+function gerarCor() {
     const num = '0123456789ABCDEF';
-    let cor = '#'
-    
-    for(let gerar=0;gerar<4;gerar+=1){
+    let cor = '#';
+    for(let gerar=0;gerar<4;gerar+=1) {
       cor += num[Math.floor(Math.random() * 16)];
     }
-    if(cor !== '#FFFFFF'){
+    if(cor !== '#FFFFFF') {
         return cor;
     }
 }
-function atribuicao(){
+function atribuicao() {
     const cor1 = document.getElementById('black').style.backgroundColor='black';
-    const cor2= document.getElementById('purple').style.backgroundColor=gerarCor();
-    const cor3= document.getElementById('blue').style.backgroundColor=gerarCor();
-    const cor4=document.getElementById('green').style.backgroundColor=gerarCor();
-    const cores =[cor1,cor2,cor3,cor4];
-    const botao = cores;
-    const btn = JSON.stringify(botao);
-    window.localStorage.setItem('colorPalette',btn);
-    
-    console.log (btn)
+    const cor2 = document.getElementById('purple').style.backgroundColor=gerarCor();
+    const cor3 = document.getElementById('blue').style.backgroundColor=gerarCor();
+    const cor4 = document.getElementById('green').style.backgroundColor=gerarCor();
+    localStorage.setItem('colorPalette', JSON.stringify([cor1,cor2,cor3,cor4]));
+    window.onload = JSON.parse(localStorage.getItem('colorPalette'));
 }
-// atribuicao();
-
-
 
