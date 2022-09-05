ed53b0a33830f9a3d8a80ec25017cd971749097c
Bruna <brunaalmeida075@gmail.com>
2022-09-05 18:25:54 -0300

    resolução do requisito  11

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function criaBotao() {
     const botao = document.createElement('button');
     botao.id = 'button-random-color';
     botao.innerText = 'Cores aleatórias';
-    botao.addEventListener('click', atribuicao);
+    botao.addEventListener('click', atribuicaoCor);
     const pai = document.getElementById('color-palette');
     pai.appendChild(botao);
 }
@@ -17,14 +17,12 @@ function gerarCor() {
         return cor;
     }
 }
-function atribuicao() {
+function atribuicaoCor() {
     const cor1 = document.getElementById('black').style.backgroundColor='black';
     const cor2 = document.getElementById('purple').style.backgroundColor=gerarCor();
     const cor3 = document.getElementById('blue').style.backgroundColor=gerarCor();
     const cor4 = document.getElementById('green').style.backgroundColor=gerarCor();
     localStorage.setItem('colorPalette', JSON.stringify([cor1,cor2,cor3,cor4]));
-    // window.onload = JSON.parse(localStorage.getItem('colorPalette'));
-    console.log(JSON.stringify([cor1,cor2,cor3,cor4]))
 
 }
 function devolverCor() {
@@ -39,47 +37,32 @@ function devolverCor() {
     cor3.style.backgroundColor = cores[2];
     cor4.style.backgroundColor = cores[3];
 }
-window.onload = devolverCor()
-
-// function a (){
-
-//     let barra = document.querySelectorAll(".color");
-
-// barra.forEach((item)=>{
-
+function n () {
+    let a = document.querySelectorAll('.color');
     
-//     item.addEventListener("click", function(e) {
-//         e.preventDefault();
-      
-//         barra.forEach((i) => {
-//             if(i != this){
-
-//                 console.log(i.classList.remove("selected"));
-//             }else(console.log(i.classList.toggle('selected')))
-//         }); 
-//     })
-//   });
-// }a();
-// function n () {
-//     let a = document.querySelectorAll('.color');
-//     for (let e of a){
-//     e.addEventListener('click', function () {
-//         if( a != 'color'){
-//             e.classList.remove('selected')
-//         }
-//         e.classList.toggle('selected');
-//         // console.log(e)
-//         return e
-//     })
-//     }
+    for (let e of a){
+    e.addEventListener('click', function () {
+        if( a != 'color'){
+            e.classList.remove('selected')
+        }
+        e.classList.toggle('selected');
+        // console.log(e)
+        return e
+    })
+    }
  
-// }n ();
-
-// function limpar() {
-//     let botao = document.getElementById('clear-board');
-//     let pixels = document.querySelectorAll('.pixel');
-//     if (pixels != 'white'){
-//         console.log('diferente')
-//     }else {console.log('igual')}
-   
-// }limpar()
+}n ();
+function addClassWhite() {
+    let pixels = document.querySelectorAll('.pixel');
+    for ( let o of pixels){
+        // o.classList.toggle('white');
+        o.style.backgroundColor = '#FFFFFF'
+        return o;
+    };
+    
+}
+function limpar() {
+    const botao = document.getElementById('clear-board');
+    botao.addEventListener('click', addClassWhite());
+}limpar()
+// window.onload = devolverCor()
